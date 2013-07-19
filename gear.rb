a5ef61d96568411bf3ad762ca2c24a6f75196f9b
Gilberto Villa <ingilniero@gmail.com>
2013-07-18 22:48:04 -0500

    Isolate behavior to its own class

--- gear.rb
+++ gear.rb
@@ -1,10 +1,12 @@
+require './wheel'
+
 class Gear
   attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog, wheel = nil)
     @chainring = chainring
     @cog = cog
-    @wheel = Wheel.new(rim, tire)
+    @wheel = wheel
   end
 
   def ratio
@@ -14,13 +16,7 @@ class Gear
   def gear_inches
     ratio * wheel.diameter
   end
-
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      rim + (tire * 2)
-    end
-  end
 end
 
-puts Gear.new(52, 11, 26, 1.5).gear_inches
-puts Gear.new(52, 11, 24, 1.25).gear_inches
+puts Gear.new(52, 11, @wheel).gear_inches
+puts Gear.new(52, 11, @wheel).ratio
