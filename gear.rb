34a348d469ecb7edb96c558c1f5e4aa65b2ad159
Gilberto Villa <ingilniero@gmail.com>
2013-07-23 16:17:02 -0500

    Change Gear to only be awere of an object that responds to diameter

--- gear.rb
+++ gear.rb
@@ -1,13 +1,12 @@
 require './wheel'
 
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog, wheel )
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
+    @wheel = wheel
   end
 
   def ratio
@@ -15,9 +14,9 @@ class Gear
   end
 
   def gear_inches
-    ratio * Wheel.new(rim, tire).diameter
+    ratio * wheel.diameter
   end
 end
 
-puts Gear.new(52, 11,26,1.5).gear_inches
-puts Gear.new(52, 11,26, 1.5).ratio
+puts Gear.new(52, 11, @wheel).gear_inches
+puts Gear.new(52, 11, @wheel).ratio
