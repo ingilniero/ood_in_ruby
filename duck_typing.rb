ddfafd0acd84ad984fd8e9f2ec88c5254eacb12b
Gilberto Villa <ingilniero@gmail.com>
2013-07-27 08:57:59 -0500

    Trip coordinator and driver added to the trip preparation

--- duck_typing.rb
+++ duck_typing.rb
@@ -1,8 +1,31 @@
 class Trip
   attr_reader :bicycles, :customers, :vehicle
 
-  def prepare(mechanic)
-    mechanic.prepare_bicycles(bicycles)
+  def prepare(preparers)
+    preparers.each do |preparer|
+      case preparer
+      when Mechanic
+        preparer.prepare_bicycles(bicycles)
+      when TripCoodinator
+        preparer.buy_food(customers)
+      when Driver
+        preparer.gas_up(vehicle)
+        preparer.fill_water_tank(vehicle)
+      end
+    end
+  end
+end
+
+class TripCoordinator
+  def buy_food(customers)
+  end
+end
+
+class Driver
+  def gas_up(vehicle)
+  end
+
+  def fill_water_tank(vehicle)
   end
 end
 
@@ -14,6 +37,5 @@ class Mechanic
   end
 
   def prepare_bicycle(bicycle)
-
   end
 end
