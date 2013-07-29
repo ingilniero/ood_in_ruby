ba8a77838e2d9fbad030ee9f43e1528bdb843ce9
Gilberto Villa <ingilniero@gmail.com>
2013-07-28 23:10:35 -0500

    Misapplying inheritence

--- inheritance.rb
+++ inheritance.rb
@@ -30,3 +30,21 @@ end
 bike = Bicycle.new style: :mountain, size: 'S', front_shock: 'Manitou', rear_shock: 'Fox'
 puts bike.size
 puts bike.spares
+
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock)
+  end
+end
+
+mountain_bike = MountainBike.new style: :road, size: 'S', front_shock: 'Manitou', rear_shock: 'Fox'
+puts mountain_bike.size
+puts mountain_bike.spares
