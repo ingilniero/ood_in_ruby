7915625a705d90b788881fc1b576bb012afdd798
Gilberto Villa <ingilniero@gmail.com>
2013-07-28 23:26:57 -0500

    Separating abstract from concrete

--- inheritance.rb
+++ inheritance.rb
@@ -1,8 +1,10 @@
 class Bicycle
-  attr_reader :size
+  attr_reader :size, :chain, :tire_size
 
   def initialize(args = {})
     @size = args[:size]
+    @chain = args[:chain]
+    @tire_size = args[:tire_size]
   end
 
 end
@@ -11,7 +13,7 @@ class RoadBike < Bicycle
   attr_reader :tape_color
 
   def initialize(args)
-    @tape_collor = args[:tape_color]
+    @tape_color = args[:tape_color]
     super(args)
   end
 
