c9e492597dfaff55ab8e050d65f1077babc13479
Gilberto Villa <ingilniero@gmail.com>
2013-07-28 23:23:35 -0500

    Promoting abstract behavior

--- inheritance.rb
+++ inheritance.rb
@@ -1,36 +1,26 @@
 class Bicycle
-  def initialize(args = {})
+  attr_reader :size
 
+  def initialize(args = {})
+    @size = args[:size]
   end
 
 end
 
 class RoadBike < Bicycle
-  attr_reader :style, :size, :tape_color,
-            :front_shock, :rear_shock
+  attr_reader :tape_color
 
   def initialize(args)
-    @style = args[:style]
-    @size = args[:size]
     @tape_collor = args[:tape_color]
-    @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
+    super(args)
   end
 
   def spares
-    if style == :road
-      {
-        chain: '10-speed',
-        tire_size: '23',
-        tape_color: tape_color
-      }
-    else
-      {
-        chain: '10-speed',
-        tire_size: '2.1',
-        rear_shock: rear_shock
-      }
-    end
+    {
+      chain: '10-speed',
+      tire_size: '23',
+      tape_color: tape_color
+    }
   end
 end
 
