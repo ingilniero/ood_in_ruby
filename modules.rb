e74902479b7f9943aa3ddbe8a1814b422d7c78c9
Gilberto Villa <ingilniero@gmail.com>
2013-07-29 23:17:12 -0500

    Include module in classes than can be scheduled

--- modules.rb
+++ modules.rb
@@ -35,9 +35,31 @@ class Bicycle
   end
 end
 
+class Vehicle
+  include Schedulable
+
+  def lead_days
+    3
+  end
+end
+
+class Mechanic
+  include Schedulable
+
+  def lead_days
+    4
+  end
+end
+
 require 'date'
 starting = Date.parse("2015/09/04")
 ending = Date.parse("2015/09/10")
 
 b = Bicycle.new
 puts b.schedulable?(starting, ending)
+
+v = Vehicle.new
+v.schedulable?(starting, ending)
+
+m = Mechanic.new
+m.schedulable?(starting, ending)
